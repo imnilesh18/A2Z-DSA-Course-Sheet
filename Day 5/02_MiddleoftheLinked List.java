51d6856abbe16f212a65d19f654333427b492a6c
Nilesh <nileshshrivastava7737@gmail.com>
2024-08-04 15:55:58 +0530

    LL

---
+++ Day 5/02_MiddleoftheLinked List.java
@@ -0,0 +1,81 @@
+/*
+ * 876. Middle of the Linked List
+ * Given the head of a singly linked list, return the middle node of the linked
+ * list.
+ * 
+ * If there are two middle nodes, return the second middle node.
+ * 
+ * Example 1:
+ * Input: head = [1,2,3,4,5]
+ * Output: [3,4,5]
+ * Explanation: The middle node of the list is node 3.
+ * 
+ * Example 2:
+ * Input: head = [1,2,3,4,5,6]
+ * Output: [4,5,6]
+ * Explanation: Since the list has two middle nodes with values 3 and 4, we
+ * return the second one.
+ */
+
+// The Tortoise and Hare algorithm 
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+
+// Time Complexity: O(N/2)
+// Space Complexity : O(1)
+
+// Brute Force:
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp = head;
+        int cnt = 0;
+        while (temp != null) {
+            cnt++;
+            temp = temp.next;
+        }
+
+        int middleNode = (cnt / 2) + 1;
+
+        temp = head;
+        while (temp != null) {
+            middleNode--;
+            if (middleNode == 0) {
+                break;
+            }
+            temp = temp.next;
+        }
+        return temp;
+    }
+}
+// TC - O(N+N/2)
+// SC - O(1)
