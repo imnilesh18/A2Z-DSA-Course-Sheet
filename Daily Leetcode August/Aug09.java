b8fdcb89e4bb4cd3680fbc658ea449d7efa117ed
Nilesh <nileshshrivastava7737@gmail.com>
2024-08-10 00:09:59 +0530

    Aug09

--- Daily Leetcode August/Aug09.java
+++ Daily Leetcode August/Aug09.java
@@ -21,6 +21,64 @@
  * Output: 0
  */
 
+// Other Approach:
+class Solution {
+
+    public boolean isMagicGrid(int[][] grid, int r, int c) {
+        // Uniqueness 1 to 9 and distinct
+        Set<Integer> st = new HashSet<>();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                int num = grid[r + i][c + j];
+                if (num < 1 || num > 9 || st.contains(num)) {
+                    return false;
+                } else {
+                    st.add(num);
+                }
+            }
+        }
+
+        // Check Sum - Rows and Columns
+        int SUM = grid[r][c] + grid[r][c + 1] + grid[r][c + 2];
+        // int SUM = 15;
+        for (int i = 0; i < 3; i++) {
+            if (grid[r + i][c] + grid[r + i][c + 1] + grid[r + i][c + 2] != SUM) {
+                return false;
+            }
+            if (grid[r][c + i] + grid[r + 1][c + i] + grid[r + 2][c + i] != SUM) {
+                return false;
+            }
+        }
+
+        // Diagonal Sum
+        if (grid[r][c] + grid[r + 1][c + 1] + grid[r + 2][c + 2] != SUM) {
+            return false;
+        }
+        // Anti Diagonal Sum
+        if (grid[r][c + 2] + grid[r + 1][c + 1] + grid[r + 2][c] != SUM) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public int numMagicSquaresInside(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        int count = 0;
+
+        for (int i = 0; i <= rows - 3; i++) {
+            for (int j = 0; j <= cols - 3; j++) {
+                if (isMagicGrid(grid, i, j)) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
+
 // Brute Force:
 class Solution {
     public boolean checkRows(int[][] mat) {
