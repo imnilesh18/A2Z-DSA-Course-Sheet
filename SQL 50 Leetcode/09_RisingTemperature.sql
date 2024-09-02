a04cc2eb9873c060c7b009ac49a83f2b6795a0f8
Nilesh <nileshshrivastava7737@gmail.com>
2024-09-02 11:19:52 +0530

    197. Rising Temperature

---
+++ SQL 50 Leetcode/09_RisingTemperature.sql
@@ -0,0 +1,79 @@
+197. Rising Temperature
+
+Table: Weather
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| recordDate    | date    |
+| temperature   | int     |
++---------------+---------+
+id is the column with unique values for this table.
+There are no different rows with the same recordDate.
+This table contains information about the temperature on a certain day.
+
+Write a solution to find all dates' id with higher temperatures compared to its previous dates (yesterday).
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Weather table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Output: 
++----+
+| id |
++----+
+| 2  |
+| 4  |
++----+
+Explanation: 
+In 2015-01-02, the temperature was higher than the previous day (10 -> 25).
+In 2015-01-04, the temperature was higher than the previous day (20 -> 30).
+
+Solution:
+
+w1 table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+
+w2 table:
++----+------------+-------------+
+| id | recordDate | temperature |
++----+------------+-------------+
+| 1  | 2015-01-01 | 10          |
+| 2  | 2015-01-02 | 25          |
+| 3  | 2015-01-03 | 20          |
+| 4  | 2015-01-04 | 30          |
++----+------------+-------------+
+Approach 1:
+
+SELECT w1.id
+FROM weather w1, weather w2
+WHERE datediff(w1.recorddate, w2.recorddate) = 1 -- Compares the dates to find consecutive days
+  AND w1.temperature > w2.temperature;            -- Checks if the temperature of the current day is higher than the previous day
+
+Approach 2:
+
+SELECT w1.id
+FROM weather w1 
+JOIN weather w2 
+ON subdate(w1.recorddate, 1) = w2.recorddate  -- Joins the Weather table on consecutive dates by subtracting 1 day from w1's date and matching it to w2's date
+AND w1.temperature > w2.temperature;           -- Checks if the temperature of the current day (w1) is higher than the temperature of the previous day (w2)
