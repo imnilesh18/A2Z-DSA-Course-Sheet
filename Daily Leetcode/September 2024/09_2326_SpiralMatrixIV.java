b468ef9f225f61068965034bc5d7dcd291dabee8
Nilesh <nileshshrivastava7737@gmail.com>
2024-09-09 19:06:09 +0530

    326. Spiral Matrix IV

--- Daily Leetcode/September 2024/09_2326_SpiralMatrixIV.java
+++ Daily Leetcode/September 2024/09_2326_SpiralMatrixIV.java
@@ -42,6 +42,69 @@
  */
 class Solution {
     public int[][] spiralMatrix(int m, int n, ListNode head) {
+        int left = 0; 
+        int top = 0;
+        int bottom = m - 1;
+        int right = n - 1;
+        ListNode curr = head;
+        int[][] result = new int[m][n];
+
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                result[i][j] = -1;
+            }
+        }
+
+        while(top <= bottom && left <= right){
+
+            for(int i = left; curr != null && i<= right; i++){
+                result[top][i] = curr.val;
+                curr = curr.next;
+            }
+            top++;
+
+            for(int i = top; curr != null && i <= bottom; i++){
+                result[i][right] = curr.val;
+                curr = curr.next;
+            }
+            right--;
+
+
+            if (top <= bottom) {
+                for (int i = right; curr != null && i >= left; i--) {
+                    result[bottom][i] = curr.val;
+                    curr = curr.next;
+                    
+                }
+                bottom--;
+            }
+
+            if (left <= right) {
+                for (int i = bottom; curr != null && i >= top; i--) {
+                    result[i][left] = curr.val;
+                    curr = curr.next;
+                }
+                left++;
+            }
+        }
+        return result;
+    }
+}
+
+// Other Approach: 
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[][] spiralMatrix(int m, int n, ListNode head) {
         int left = 0;
         int top = 0;
         int bottom = m - 1;
