be763ad6fe27f8bcf6de76a19407e92848f7f712
Nilesh <nileshshrivastava7737@gmail.com>
2024-10-04 23:14:14 +0530

    2491. Divide Players Into Teams of Equal Skill

--- Daily Leetcode/October 2024/04_2491_DividePlayersIntoTeamsOfEqualSkill.java
+++ Daily Leetcode/October 2024/04_2491_DividePlayersIntoTeamsOfEqualSkill.java
@@ -40,6 +40,8 @@
  */
 
 // My approach:
+//T.C : O(nlogn)
+//S.C : O(1)
 class Solution {
     public long dividePlayers(int[] skill) {
         int n = skill.length;
@@ -60,5 +62,47 @@ class Solution {
         }
 
         return result;
+    }
+}
+
+// Approach-2 (Using frequency of skills similar to counting sort)
+// T.C : O(n)
+// S.C : O(1000) ~= O(1)
+class Solution {
+    public long dividePlayers(int[] skill) {
+        int n = skill.length;
+
+        // Frequency array to count occurrences of each skill value
+        int[] freq = new int[1001];
+        int totalSum = 0;
+
+        for (int s : skill) {
+            totalSum += s;
+            freq[s]++; // Update frequency
+        }
+
+        int teams = n / 2;
+
+        // Check if total sum is divisible by the number of teams
+        if (totalSum % teams != 0) {
+            return -1;
+        }
+
+        int targetSum = totalSum / teams;
+        long chemistry = 0;
+
+        for (int currSkill : skill) {
+            int remainingSkill = targetSum - currSkill;
+
+            // If the complementary skill is not available, return -1
+            if (freq[remainingSkill] <= 0) {
+                return -1;
+            }
+
+            chemistry += (long) currSkill * (long) remainingSkill;
+            freq[remainingSkill]--; // Reduce the frequency of used skill
+        }
+
+        return chemistry / 2;
     }
 }
