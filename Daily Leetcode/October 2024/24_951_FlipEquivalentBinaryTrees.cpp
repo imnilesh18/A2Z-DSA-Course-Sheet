3c239fda5b1124e13b22f5672380b7606a0f14f9
Nilesh <nileshshrivastava7737@gmail.com>
2024-10-24 20:20:16 +0530

    951. Flip Equivalent Binary Trees

--- Daily Leetcode/October 2024/24_951_FlipEquivalentBinaryTrees.cpp
+++ Daily Leetcode/October 2024/24_951_FlipEquivalentBinaryTrees.cpp
@@ -27,3 +27,35 @@ Each tree will have unique node values in the range [0, 99].
 // Approach - Simple Recursion
 // T.C : ~O(4*n)
 // S.C : O(n) system stack space because of Recursion
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        
+        if (root1 == NULL && root2 == NULL) {
+            return true;
+        } 
+
+        if (root1 == NULL || root2 == NULL) {
+            return false;
+        }
+
+        if (root1->val == root2 ->val) {
+            bool withoutFlip = flipEquiv(root1->left, root2->left) && flipEquiv(root1->right, root2->right);
+            bool withFlip = flipEquiv(root1->left, root2->right) && flipEquiv(root1->right, root2->left);
+            return withoutFlip || withFlip;
+        } else {
+            return false;
+        }
+    }
+};
