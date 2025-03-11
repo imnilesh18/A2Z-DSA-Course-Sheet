e207c1afba8130ebc2cf533a63a883d7c47a0dcd
imnilesh18 <nileshshrivastava7737@gmail.com>
2025-03-12 00:50:28 +0530

    1358. Number of Substrings Containing All Three Characters

--- Sliding WIndow/1358_Number of Substrings Containing All Three Characters.cpp
+++ Sliding WIndow/1358_Number of Substrings Containing All Three Characters.cpp
@@ -1,6 +1,6 @@
 /*
  * 1358. Number of Substrings Containing All Three Characters
- * After this solve: 306. Count of Substrings Containing Every Vowel and K Consonants II
+ * After this solve: 3306. Count of Substrings Containing Every Vowel and K Consonants II
  *
  * Given a string s consisting only of characters a, b and c.
  * Return the number of substrings containing at least one occurrence of all these characters a, b and c.
